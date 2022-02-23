1ff43bbc81b8eb9ad8cbbb1b0e19ef0e77154f0f
Hiren <hiren@freshbits.in>
2022-02-23 16:29:19 +0530

    Setup tailwind and add properties data

---
+++ properties.js
@@ -0,0 +1,194 @@
+var properties = [
+    {
+        "name": "Suvarna Bhoomi",
+        "slug": "suvarna-bhoomi",
+        "description": "Residential project, Khushi Palm Universe in Rajkot is offering units for sale in Munjka. Check out some Apartment that suit your lifestyle and liking. Possession date of Khushi Palm Universe is Apr, 2022. The property offers 3 BHK, 4 BHK units. As per the area plan, units are in the size of 812.24 sq.ft.. It has 120 units. There are 2 buildings in this project. Contact for further details. Khushi Palm Universe is located in Palm Heights, 80 Feet Ring Road, Gangotri Park Main Road, Behind Palm City, Raiya Road, Nana Mava. In terms of facilities, Khushi Palm Universe is loaded with multiple offerings such as Lift, Gymnasium, Power Backup. For families with kids, there is Children's Play Area, nearby apart from Indoor Game Room - Billiards/Snooker Table, Carom Board, Table Tennis, Foosball Table, Swimming Pool. Residents can make use of Amphitheatre, Library, provisions in the project. It is an Earthquake Resistant Structure. Khushi Palm Universe follows all rules as prescribed by the state RERA. All details are furnished on the RERA portal as well.ID is PR/GJ/RAJKOT/RAJKOT/Others/RAA01553/070218. Khushi Developers is a very well-known developer-firm in this real estate market. Residents and their lifestyle are at the centre of their developments. Prominent suburbs of Rajkot are close by to Munjka and with several schools, hospitals, banks and offices situated in the proximity, the project is a preferred choice for home seekers.",
+        "amenities": [
+            {
+                "name": "Children's Play Area",
+                "icon": "children-play-area.png"
+            }, {
+                "name": "Indoor Games",
+                "icon": "indoor-games.png"
+            }, {
+                "name": "Club House",
+                "icon": "club-house.png"
+            }, {
+                "name": "CCTV",
+                "icon": "cctv.png"
+            }, {
+                "name": "Skating Rink",
+                "icon": "skating-rink.png"
+            }
+        ],
+        "amenities_details": {
+            "floorings": [
+                {
+                    "name": "Living/Dining",
+                    "details": "Anti Skid Tiles"
+                }, {
+                    "name": "Master Bedroom",
+                    "details": "Anti Skid Tiles"
+                }, {
+                    "name": "Other Bedroom",
+                    "details": "Anti Skid Tiles"
+                }
+            ],
+            "fittings": [
+                {
+                    "name": "Electrical",
+                    "details": "Concealed Copper Wiring"
+                }, {
+                    "name": "Toilets",
+                    "details": "Branded CP Fitting"
+                }, {
+                    "name": "Kitchen",
+                    "details": "C.P Fittings of JAQUA"
+                }, {
+                    "name": "Doors",
+                    "details": "Decorative Door with Quality"
+                }, {
+                    "name": "Windows",
+                    "details": "Aluminium Powder Coated"
+                }
+            ]
+        },
+        "number_of_kitchens": 1,
+        "number_of_balcony": 1,
+        "number_of_living_area": 1,
+        "number_of_storage_room": 1,
+        "number_of_buildings": 7,
+        "number_of_units": 392,
+        "latitude": 22.2793087,
+        "longitude": 70.7979746,
+        "address": "Speedwell Party Plot Chowk, Ambika Township Main Road, Near VYO Haveli, Rajkot",
+        "brochure": "suvarna-bhoomi/suvarna-bhoomi-brochure.pdf",
+        "rera_id": "PR/GJ/RAJKOT/RAJKOT/Others/MAA05205/040419",
+        "possession_start_date": "March-2022",
+        "possession_end_date": "March-2023",
+        "current_status": "Under Construction",
+        "media": [{
+            "name": "suvarna-bhoomi/main-view.jpg"
+        }, {
+            "name": "suvarna-bhoomi/main-view.mp4"
+        }],
+        "bedroom_details": [
+            {
+                "number_of_bedrooms": 3,
+                "carpet_areas": [{
+                    "carpet": "1100",
+                    "number_of_kitchens": 1,
+                    "number_of_balcony": 1,
+                    "number_of_living_area": 1,
+                    "media": "suvarna-bhoomi/1100-carpet.jpg"
+                }, {
+                    "carpet": "1275",
+                    "number_of_kitchens": 1,
+                    "number_of_balcony": 1,
+                    "number_of_living_area": 1,
+                    "media": "suvarna-bhoomi/1275-carpet.jpg"
+                }]
+            }, {
+                "number_of_bedrooms": 4,
+                "carpet_areas": [{
+                    "carpet": "2015",
+                    "number_of_kitchens": 1,
+                    "number_of_balcony": 1,
+                    "number_of_living_area": 1,
+                    "media": "suvarna-bhoomi/2015-carpet.jpg"
+                }]
+            }
+        ]
+    }, {
+        "name": "Khushi Palm Universe",
+        "slug": "khushi-palm-universe",
+        "description": "Residential project, Khushi Palm Universe in Rajkot is offering units for sale in Munjka. Check out some Apartment that suit your lifestyle and liking. Possession date of Khushi Palm Universe is Apr, 2022. The property offers 3 BHK, 4 BHK units. As per the area plan, units are in the size of 812.24 sq.ft.. It has 120 units. There are 2 buildings in this project. Contact for further details. Khushi Palm Universe is located in Palm Heights, 80 Feet Ring Road, Gangotri Park Main Road, Behind Palm City, Raiya Road, Nana Mava. In terms of facilities, Khushi Palm Universe is loaded with multiple offerings such as Lift, Gymnasium, Power Backup. For families with kids, there is Children's Play Area, nearby apart from Indoor Game Room - Billiards/Snooker Table, Carom Board, Table Tennis, Foosball Table, Swimming Pool. Residents can make use of Amphitheatre, Library, provisions in the project. It is an Earthquake Resistant Structure. Khushi Palm Universe follows all rules as prescribed by the state RERA. All details are furnished on the RERA portal as well.ID is PR/GJ/RAJKOT/RAJKOT/Others/RAA01553/070218. Khushi Developers is a very well-known developer-firm in this real estate market. Residents and their lifestyle are at the centre of their developments. Prominent suburbs of Rajkot are close by to Munjka and with several schools, hospitals, banks and offices situated in the proximity, the project is a preferred choice for home seekers.",
+        "amenities": [
+            {
+                "name": "Children's Play Area",
+                "icon": "children-play-area.png"
+            }, {
+                "name": "Indoor Games",
+                "icon": "indoor-games.png"
+            }, {
+                "name": "Club House",
+                "icon": "club-house.png"
+            }, {
+                "name": "CCTV",
+                "icon": "cctv.png"
+            }, {
+                "name": "Skating Rink",
+                "icon": "skating-rink.png"
+            }
+        ],
+        "amenities_details": {
+            "floorings": [
+                {
+                    "name": "Living/Dining",
+                    "details": "Anti Skid Tiles"
+                }, {
+                    "name": "Master Bedroom",
+                    "details": "Anti Skid Tiles"
+                }, {
+                    "name": "Other Bedroom",
+                    "details": "Anti Skid Tiles"
+                }
+            ],
+            "fittings": [
+                {
+                    "name": "Electrical",
+                    "details": "Concealed Copper Wiring"
+                }, {
+                    "name": "Toilets",
+                    "details": "Branded CP Fitting"
+                }, {
+                    "name": "Kitchen",
+                    "details": "C.P Fittings of JAQUA"
+                }, {
+                    "name": "Doors",
+                    "details": "Decorative Door with Quality"
+                }, {
+                    "name": "Windows",
+                    "details": "Aluminium Powder Coated"
+                }
+            ]
+        },
+        "number_of_storage_room": 0,
+        "number_of_buildings": 2,
+        "number_of_units": 120,
+        "latitude": 22.3092134,
+        "longitude": 70.8215963,
+        "address": "80 Feet Road Or 50 Feet Road Corner, Near Somnath Residency, Kuvadava Road, Shree Ram Society, Rajkot",
+        "brochure": "khushi-palm-universe/khushi-palm-universe-brochure.pdf",
+        "rera_id": "PR/GJ/RAJKOT/RAJKOT/Others/MAA05205/040419",
+        "possession_start_date": "March-2022",
+        "possession_end_date": "March-2023",
+        "current_status": "Ready To Move",
+        "media": [{
+            "name": "khushi-palm-universe/main-view.jpg"
+        }, {
+            "name": "khushi-palm-universe/main-view.mp4"
+        }],
+        "bedroom_details": [
+            {
+                "number_of_bedrooms": 3,
+                "carpet_areas": [{
+                    "carpet": "812",
+                    "number_of_kitchens": 1,
+                    "number_of_balcony": 1,
+                    "number_of_living_area": 1,
+                    "media": "khushi-palm-universe/812-carpet.jpg"
+                }]
+            }, {
+                "number_of_bedrooms": 5,
+                "carpet_areas": [{
+                    "carpet": "986",
+                    "number_of_kitchens": 1,
+                    "number_of_balcony": 1,
+                    "number_of_living_area": 1,
+                    "media": "khushi-palm-universe/986-carpet.jpg"
+                }]
+            }
+        ]
+    }
+]
